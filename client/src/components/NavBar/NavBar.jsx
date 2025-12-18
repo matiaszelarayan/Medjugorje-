331c7223c0a5fafbc62ab12fcf5529d9ae5c5505
Emanuel Grigolatto <ema_grigo@hotmail.com>
2025-12-18 20:05:28 -0300

    modificacion por error en mayus y minus++

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./Navbar.module.css";
+import styles from "./NavBar.module.css";
 import logo from "../../assets/logo.png";
 import { toast } from "react-toastify";
 
