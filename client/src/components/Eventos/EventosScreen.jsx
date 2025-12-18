726eae32220aa942c55324bc27793cc37846a8ea
Emanuel Grigolatto <ema_grigo@hotmail.com>
2025-12-18 20:00:44 -0300

    modificacion por error en mayus y minus+

--- client/src/components/Eventos/EventosScreen.jsx
+++ client/src/components/Eventos/EventosScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styles from "./eventos.module.css";
+import styles from "./Eventos.module.css";
 import EventoFormModal from "./EventoFormModal";
 import ConfirmDeleteModal from "../ConfirmDeleteModal/ConfirmDeleteModal";
 import { Pencil, Trash2 } from "lucide-react";
